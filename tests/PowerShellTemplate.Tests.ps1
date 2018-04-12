0057f385976ad6aa9f219e5fbf53f9cbaeb229cb
James Ruskin <james.ruskin@questionmark.com>
2018-04-12 18:33:59 +0100

    Remove $global variables (and PSSA workaround) as unrequired

--- tests/PowerShellTemplate.Tests.ps1
+++ tests/PowerShellTemplate.Tests.ps1
@@ -1,16 +1,13 @@
-﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidGlobalVars", "")]
-param()
+﻿$ModuleUnderTest = Split-Path (Split-Path $PSScriptRoot -Parent) -Leaf
 
-$global:ModuleUnderTest = Split-Path (Split-Path $PSScriptRoot -Parent) -Leaf
-
-if (!(Get-Module $global:ModuleUnderTest -ErrorAction SilentlyContinue)) {
-    Import-Module $global:ModuleUnderTest -Scope Global
+if (!(Get-Module $ModuleUnderTest -ErrorAction SilentlyContinue)) {
+    Import-Module $ModuleUnderTest -Scope Global
 }
 
 if ($QMBuild = Get-Module QMBuild -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1) {
-    Write-Verbose "Running Script Analyzer common tests for $($global:ModuleUnderTest)"
+    Write-Verbose "Running Script Analyzer common tests for $($ModuleUnderTest)"
     $CommonTests = Join-Path $QMBuild.ModuleBase CommonTests\Module.Tests.ps1
     if (Test-Path $CommonTests) {
-        . $CommonTests
+        . $CommonTests -ModuleUnderTest $ModuleUnderTest
     }
 }
